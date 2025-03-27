3595e036a74216a64b9fe038295995c746898d90
あめ <100082890+666yunoGASAI@users.noreply.github.com>
2025-03-27 14:40:37 +0800

    Update app.py

--- app.py
+++ app.py
@@ -13,7 +13,6 @@ from sentiment_analysis import (
     analyze_for_trolling
 )
 # Import Tagalog sentiment functions
-#import
 from tagalog_sentiment import (
     is_tagalog,
     get_tagalog_sentiment_breakdown
